828800fc18206caa0d89ac46cb60086d6ca50866
VetleFell <vetle.stubberud@gmail.com>
2022-04-06 18:25:18 +0300

    refactor/change text to new values in codepen

--- src/components/display/Typography/stories.tsx
+++ src/components/display/Typography/stories.tsx
@@ -9,7 +9,7 @@ export default {
   argTypes: {
     variant: {
       control: "select",
-      options: ["h1", "h2", "h3", "h4", "h5", "h6", "body1"],
+      options: ["h1", "h2", "h3", "h4", "h5", "body"],
     },
     weight: {
       control: "text",
@@ -30,52 +30,8 @@ export default {
 
 const Template: ComponentStory<typeof Typography> = (args) => <Typography {...args} />;
 
-export const H1 = Template.bind({});
-H1.args = {
+export const Text = Template.bind({});
+Text.args = {
   children: "Example Title",
   variant: "h1",
-};
-
-export const H2 = Template.bind({});
-H2.args = {
-  children: "Example Title",
-  variant: "h2",
-};
-
-export const H3 = Template.bind({});
-H3.args = {
-  children: "Example Title",
-  variant: "h3",
-};
-
-export const H4 = Template.bind({});
-H4.args = {
-  children: "Example Title",
-  variant: "h4",
-};
-
-export const H5 = Template.bind({});
-H5.args = {
-  children: "Example Title",
-  variant: "h5",
-};
-
-export const H6 = Template.bind({});
-H6.args = {
-  children: "Example Title",
-  variant: "h6",
-};
-
-export const body = Template.bind({});
-body.args = {
-  children: "Example body text",
-  variant: "body",
-};
-
-export const number = Template.bind({});
-number.args = {
-  children: 10000000,
-  // @ts-ignore todo: no body2
-  variant: "body2",
-  style: "number",
 };
