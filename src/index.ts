1e226255c3fe0db6b1c9f8cb2b028b955254b0b3
German D. Schneck <german@zignaly.com>
2022-04-19 14:13:17 -0300

    hf(develop): organize exports on main file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 // Display
-import Avatar from "./components/display/Avatar";
-import Typography from "./components/display/Typography";
-import Table from "./components/display/Table";
-import Loader from "./components/display/Loader";
-import ZigWalletIndicator from "./components/display/ZigWalletIndicator";
-import Tabs from "./components/navigation/Tabs";
-import Tab from "./components/navigation/Tabs/components/Tab";
-import TabPanel from "./components/navigation/Tabs/components/TabPanel";
+import Avatar from "components/display/Avatar";
+import ErrorMessage from "components/display/ErrorMessage";
+import Loader from "components/display/Loader";
+import Table from "components/display/Table";
+import Typography from "components/display/Typography";
+import ZigWalletIndicator from "components/display/ZigWalletIndicator";
 
 // Inputs
-import Button from "./components/inputs/Button";
+import Button from "components/inputs/Button";
+import CheckBox from "components/inputs/Button";
+import IconButton from "components/inputs/Button";
+import InputAmount from "components/inputs/Button";
+import InputText from "components/inputs/Button";
+import Selector from "components/inputs/Button";
+
+// Navigation
+import Tabs from "components/navigation/Tabs";
 
 export default {
   /**
@@ -20,16 +26,20 @@ export default {
 
   // Display
   Avatar,
-  Typography,
-  Table,
-  ZigWalletIndicator,
   Loader,
+  Table,
+  Typography,
+  ErrorMessage,
+  ZigWalletIndicator,
 
   // Inputs
   Button,
+  CheckBox,
+  IconButton,
+  InputAmount,
+  InputText,
+  Selector,
 
   // Navigation
   Tabs,
-  Tab,
-  TabPanel,
 };
