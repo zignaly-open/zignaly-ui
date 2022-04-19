fba67be1ba10e0879c0aaf33ade43d60c71549fb
VetleFell <vetle.stubberud@gmail.com>
2022-04-19 15:21:34 +0200

    refactor: added back lost code

---
+++ src/components/inputs/TextButton/stories.tsx
@@ -0,0 +1,52 @@
+// Dependencies
+import * as React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+// Component
+import TextButton from "./";
+
+// Assets
+import CloseIcon from "../../../assets/icons/close-icon.svg";
+import ArrowDown from "../../../assets/icons/arrow-bottom-icon.svg";
+
+export default {
+  title: "Inputs/TextButton",
+  component: TextButton,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/VaAFxJqlZERpeEHw5S5coY/Buttons?node-id=402%3A963",
+    },
+  },
+  argTypes: {
+    rightElement: {
+      options: { Show: ArrowDown, Hide: null },
+      control: { type: "radio" },
+    },
+    leftElement: {
+      options: { Show: CloseIcon, Hide: null },
+      control: { type: "radio" },
+    },
+    caption: {
+      control: { type: "text" },
+      label: "Change Caption",
+    },
+    underline: {
+      options: [true, false],
+      control: { type: "radio" },
+    },
+    onClick: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as ComponentMeta<typeof TextButton>;
+
+const Template: ComponentStory<typeof TextButton> = (args) => <TextButton {...args} />;
+
+/// Normal Buttons
+export const PlainTextButton = Template.bind({});
+PlainTextButton.args = {
+  leftElement: CloseIcon,
+};
---
+++ src/components/inputs/TextButton/styles.ts
@@ -0,0 +1,108 @@
+// Dependencies
+import styled from "styled-components";
+import { styledIf } from "../../../utils/styled";
+
+export const Container = styled.div`
+  position: relative;
+  transition: all 0.2s linear;
+  outline: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  color: #26c4c1;
+`;
+
+export const LeftElement = styled.div`
+  z-index: 3;
+  position: relative;
+  padding-right: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RightElement = styled(LeftElement)`
+  padding-right: 0;
+  padding-left: 10px;
+  color: #26c4c1;
+`;
+
+/**
+ * Icons
+ */
+interface IconProps {
+  src: any;
+  alt: any;
+}
+
+export const Icon = styled.img<IconProps>``;
+
+export const CenterIcon = styled.img<IconProps>`
+  position: relative;
+  top: 1px;
+`;
+
+/**
+ * Layout
+ */
+interface LayoutProps {
+  withElements: Boolean;
+  underline: Boolean;
+}
+
+export const Layout = styled.button<LayoutProps>`
+  border: none;
+  cursor: pointer;
+  padding: 2px;
+  outline: none;
+  position: relative;
+  user-select: none;
+  background: transparent;
+  ${({ withElements, underline }: LayoutProps) => `
+      
+      ${Container} {
+        padding: 8px 18px;
+        min-width: 88px;  
+        font-size: 13px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 20px;
+            letter-spacing: 0px;
+            text-align: center;    
+      }
+            
+      ${styledIf(
+        withElements,
+        `
+        ${Container} {
+          padding: 8px 18px;
+        }
+      `,
+      )}
+      ${styledIf(
+        underline,
+        `
+      ${Container} { 
+            text-decoration-color: #26C4C1;
+            text-decoration-line: underline;
+         `,
+      )}
+    ${styledIf(
+      !underline,
+      `
+      ${Container} { 
+        text-decoration-line: none;
+         `,
+    )}
+      
+      ${LeftElement} {
+        img {
+          width: 8px;
+          height: 8px;
+        }
+      }
+      
+  `}
+`;
