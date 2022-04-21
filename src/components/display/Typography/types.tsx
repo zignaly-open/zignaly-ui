a83e37e3819366fc154b6f36beb1f7f29637e53f
Xzirez <33001507+Xzirez@users.noreply.github.com>
2022-04-21 11:43:39 +0200

    refactor(typography): add comments from chromatic (#28)

--- src/components/display/Typography/types.tsx
+++ src/components/display/Typography/types.tsx
@@ -5,10 +5,10 @@ export const componentByVariants = {
   h2: "h2",
   h3: "h3",
   h4: "h4",
-  h5: "h5",
-  h6: "h6",
-  body: "span",
-  number: "span",
+  h5: "h5",  
+  bigNumber: "span",
+  body1: "span",
+  body2: "span"
 };
 
 export enum weightByType {
