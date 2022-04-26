92ba144c7bf241ea9433107eb0f33d41ba4cc893
Christophe Wagner <chris.wagn22@gmail.com>
2022-04-26 22:11:56 +0700

    Add svgr for storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -10,6 +10,10 @@ module.exports = async ({ config }) => {
       utils: resolve("../src/utils"),
     },
   });
+
+  // Overwrite svg rule to use svgr
+  const fileLoaderRule = config.module.rules.find((rule) => rule.test && rule.test.test(".svg"));
+  fileLoaderRule.exclude = /svg$/;
   config.module.rules.push({
     test: /\.svg$/,
     use: ["@svgr/webpack"],
