3c4a15403ab3e558d68158290006dc8b1fc60e6b
Ruben Romero Diaz <r.romero@dogoodpeople.com>
2022-04-27 16:42:30 +0200

    component_icons: update SVGr to work and typescript-lint

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -13,9 +13,17 @@ module.exports = async ({ config }) => {
 
   // Overwrite svg rule to use svgr
   const fileLoaderRule = config.module.rules.find((rule) => rule.test && rule.test.test(".svg"));
-  fileLoaderRule.exclude = /svg$/;
+  fileLoaderRule.exclude = /\.svg$/i;
+  console.log('config', config.module.rules);
   config.module.rules.push({
-    test: /\.svg$/,
+    test: /\.svg$/i,
+    type: 'asset',
+    resourceQuery: /url/, // *.svg?url
+  });
+  config.module.rules.push({
+    test: /\.svg$/i,
+    issuer: /\.[jt]sx?$/,
+    resourceQuery: { not: [/url/] },
     use: ["@svgr/webpack"],
   });
   return config;
