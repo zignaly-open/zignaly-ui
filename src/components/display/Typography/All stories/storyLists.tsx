35c0808e7e6cfce6e4464fe476a17c7be0dc736b
VetleFell <vetle.stubberud@gmail.com>
2022-04-27 16:18:36 +0200

    feat(typography): add all stories

--- src/components/display/Typography/All stories/storyLists.tsx
+++ src/components/display/Typography/All stories/storyLists.tsx
@@ -3,22 +3,84 @@ import React from "react";
 import Typography from "..";
 
 // Types
-import { componentByVariants } from "../types";
+import { componentByVariants, weightByType } from "../types";
+import { Layout, StoryList } from "./styles";
 
 type TextVariationsTypes = {
-  texts: {
+  textsRow1: {
     variant?: keyof typeof componentByVariants;
+    weight?: keyof typeof weightByType;
+  }[];
+  textsRow2?: {
+    variant?: keyof typeof componentByVariants;
+    weight?: keyof typeof weightByType;
+  }[];
+  textsRow3?: {
+    variant?: keyof typeof componentByVariants;
+    weight?: keyof typeof weightByType;
+  }[];
+  textsRow4?: {
+    variant?: keyof typeof componentByVariants;
+    weight?:keyof typeof weightByType;
+  }[];
+  textsRow5?: {
+    variant?: keyof typeof componentByVariants;
+    weight?: keyof typeof weightByType;
   }[];
 };
 
-export function TextVariations({ texts }: TextVariationsTypes) {
+export function TextVariations({
+  textsRow1,
+  textsRow2,
+  textsRow3,
+  textsRow4,
+  textsRow5,
+}: TextVariationsTypes) {
   return (
-    <div className="text-variation-list">
-      {texts.map((text, index: number) => (
-        <Typography key={`--${index.toString()}`} variant={text.variant}>
-          Example Text
-        </Typography>
-      ))}
-    </div>
+    <Layout>
+      <StoryList>
+        {textsRow1.map((text, index: number) => (
+          <Typography key={`--${index.toString()}`} variant={text.variant} weight={text.weight}>
+            Example Text
+          </Typography>
+        ))}
+      </StoryList>
+      {textsRow2 && (
+        <StoryList>
+          {textsRow2.map((text, index: number) => (
+            <Typography key={`--${index.toString()}`} variant={text.variant} weight={text.weight}>
+              Example Text
+            </Typography>
+          ))}
+        </StoryList>
+      )}
+      {textsRow3 && (
+        <StoryList>
+          {textsRow3.map((text, index: number) => (
+            <Typography key={`--${index.toString()}`} variant={text.variant} weight={text.weight}>
+              Example Text
+            </Typography>
+          ))}
+        </StoryList>
+      )}
+      {textsRow4 && (
+        <StoryList>
+          {textsRow4.map((text, index: number) => (
+            <Typography key={`--${index.toString()}`} variant={text.variant} weight={text.weight}>
+              Example Text
+            </Typography>
+          ))}
+        </StoryList>
+      )}
+      {textsRow5 && (
+        <StoryList>
+          {textsRow5.map((text, index: number) => (
+            <Typography key={`--${index.toString()}`} variant={text.variant} weight={text.weight}>
+              Example Text
+            </Typography>
+          ))}
+        </StoryList>
+      )}
+    </Layout>
   );
 }
