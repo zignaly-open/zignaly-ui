6e6f19fb07f4b0bbcc9312b140d5cbba32b5738e
VetleFell <vetle.stubberud@gmail.com>
2022-05-16 16:15:16 +0200

    refactor: percentageIndicator variations for userDashboard

--- src/components/display/Table/components/PercentageIndicator/stories.tsx
+++ src/components/display/Table/components/PercentageIndicator/stories.tsx
@@ -21,10 +21,30 @@ export const Positive = Template.bind({});
 Positive.args = {
   value: 50,
   theme: dark,
+  dashboardType: "investor",
 };
 
 export const Negative = Template.bind({});
 Negative.args = {
   value: -50,
   theme: dark,
+  dashboardType: "investor",
+};
+
+export const NegativeUserDashboard = Template.bind({});
+NegativeUserDashboard.args = {
+  value: -50,
+  subtitle: "2.2 years",
+  theme: dark,
+  withoutIcon: true,
+  dashboardType: "user",
+};
+
+export const PositiveUserDashboard = Template.bind({});
+PositiveUserDashboard.args = {
+  value: -50,
+  theme: dark,
+  subtitle: "2.2 years",
+  withoutIcon: true,
+  dashboardType: "user",
 };
