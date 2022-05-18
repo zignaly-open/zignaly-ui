9ef76d08fe2dbf69760a27b6665b4d741d6c39f0
Xzirez <33001507+Xzirez@users.noreply.github.com>
2022-05-18 21:57:58 +0200

    feat: add serviceName (#110)
    Co-authored-by: VetleFell <vetle.stubberud@gmail.com>

---
+++ src/components/display/Table/components/ServiceName/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Row = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Column = styled.div`
+  display: grid;
+`;
+
+export const Icon = styled.div`
+  margin: 0px 16px;
+  width: 36px;
+  height: 36px;
+`;
