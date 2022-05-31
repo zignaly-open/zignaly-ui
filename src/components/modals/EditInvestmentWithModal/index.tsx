58ce666ca6a203b2415afb5875473f71faf7af8a
German D. Schneck <german@zignaly.com>
2022-05-31 20:30:52 -0300

    ft(modals/EditInvestmentWithModal): remove console.log

--- src/components/modals/EditInvestmentWithModal/index.tsx
+++ src/components/modals/EditInvestmentWithModal/index.tsx
@@ -193,7 +193,6 @@ function EditInvestmentWithModal({
               value={Number(getValues("withdrawPercentageValue"))}
               initialValue={Number(getValues("withdrawPercentageValue"))}
               onChange={({ reinvest, withdraw }: { reinvest: string; withdraw: string }) => {
-                console.log(reinvest);
                 setValue("reinvestPercentageValue", reinvest.toString());
                 setValue("withdrawPercentageValue", withdraw.toString());
               }}
