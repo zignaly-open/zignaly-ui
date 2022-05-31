0cb4e4c31f00ca8fdd60ff59d88df82b08911a2e
Xzirez <33001507+Xzirez@users.noreply.github.com>
2022-05-31 17:50:52 +0200

    feat: add withdraw slider variant (#136)
    Co-authored-by: VetleFell <vetle.stubberud@gmail.com>

--- src/components/display/InvestSlider/index.tsx
+++ src/components/display/InvestSlider/index.tsx
@@ -3,16 +3,26 @@ import * as React from "react";
 import { useState } from "react";
 
 // Styled Components
-import { Bar, Dot, DotContainer, Label, Layout, Line, TextContainer } from "./styles";
-import { InvestSliderProps } from "./types";
+import {
+  Bar,
+  Dot,
+  DotContainer,
+  Label,
+  Layout,
+  Line,
+  PercentContainer,
+  TextContainer,
+} from "./styles";
+import { InvestSliderProps, SliderTypes } from "./types";
 
 const InvestSlider = ({
   className,
   max = 100,
   min = 0,
   style,
-  initialValue,
+  initialValue = 30,
   onChange = () => {},
+  type = "withdraw",
 }: InvestSliderProps) => {
   const [value, setValue] = useState<number>(
     initialValue ? ((initialValue - min) / (max - min)) * 100 : 0,
@@ -49,34 +59,81 @@ const InvestSlider = ({
     };
   }, [enabled]);
 
-  return (
-    <Layout style={style} className={className}>
-      <TextContainer>
-        <Label variant="body2" weight="demibold" color="neutral200">
-          Reinvest
-        </Label>
-        <Label variant="body2" weight="demibold" color="highlighted">
-          {value}%
-        </Label>
-      </TextContainer>
-      <Bar ref={sliderRef}>
-        <Line variant="left"></Line>
-        <Line variant="middle"></Line>
-        <Line variant="right"></Line>
-        <DotContainer onMouseDown={() => setEnabled(true)} value={value}>
-          <Dot />
-        </DotContainer>
-      </Bar>
-      <TextContainer>
-        <Label variant="body2" weight="demibold" color="neutral200">
-          Withdraw
-        </Label>
-        <Label variant="body2" weight="demibold" color="highlighted">
-          {100 - value}%
-        </Label>
-      </TextContainer>
-    </Layout>
-  );
+  const DepositSlider = () => {
+    return (
+      <Layout style={style} className={className}>
+        <TextContainer>
+          <Label variant="body2" weight="demibold" color="neutral200">
+            Reinvest
+          </Label>
+          <Label variant="body2" weight="demibold" color="highlighted">
+            {value}%
+          </Label>
+        </TextContainer>
+        <Bar margin={7} ref={sliderRef}>
+          <Line variant="left"></Line>
+          <Line variant="middle"></Line>
+          <Line variant="right"></Line>
+          <DotContainer onMouseDown={() => setEnabled(true)} value={value}>
+            <Dot />
+          </DotContainer>
+        </Bar>
+        <TextContainer>
+          <Label variant="body2" weight="demibold" color="neutral200">
+            Withdraw
+          </Label>
+          <Label variant="body2" weight="demibold" color="highlighted">
+            {100 - value}%
+          </Label>
+        </TextContainer>
+      </Layout>
+    );
+  };
+
+  const WithdrawSlider = () => {
+    return (
+      <Layout style={style} className={className}>
+        <TextContainer>
+          <Label variant="body2" weight="demibold" color="neutral200">
+            0%
+          </Label>
+        </TextContainer>
+        <Bar margin={12} ref={sliderRef}>
+          <PercentContainer value={value}>
+            <Label variant="body2" weight="demibold" color="highlighted">
+              {value}%
+            </Label>
+          </PercentContainer>
+          <Line variant="left"></Line>
+          <Line variant="middle"></Line>
+          <Line variant="right"></Line>
+          <DotContainer onMouseDown={() => setEnabled(true)} value={value}>
+            <Dot />
+          </DotContainer>
+        </Bar>
+        <TextContainer>
+          <Label variant="body2" weight="demibold" color="neutral200">
+            100%
+          </Label>
+        </TextContainer>
+      </Layout>
+    );
+  };
+
+  const Slider = () => {
+    switch (type) {
+      case SliderTypes.withdraw: {
+        return <WithdrawSlider></WithdrawSlider>;
+      }
+      case SliderTypes.deposit: {
+        return <DepositSlider></DepositSlider>;
+      }
+      default:
+        return <></>;
+    }
+  };
+
+  return Slider();
 };
 
 export default InvestSlider;
