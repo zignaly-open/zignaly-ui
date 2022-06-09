ef74f6ec7959f6f30c83409dded45d943f036905
VetleFell <vetle.stubberud@gmail.com>
2022-06-09 13:57:29 +0100

    feat(display): add zignalyQRCode

---
+++ src/components/display/ZignalyQRCode/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const QRCodeContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  border-radius: 8px;
+  background: #e1e9f0;
+`;
