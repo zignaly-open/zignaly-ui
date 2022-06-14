308fc81db8ed402c60e8a00acb603c215ec73dc6
German D. Schneck <german@zignaly.com>
2022-06-14 03:55:41 -0300

    fix(auth/ui): fix margin and autofocus

--- src/components/inputs/InputCode/index.tsx
+++ src/components/inputs/InputCode/index.tsx
@@ -1,5 +1,5 @@
 // Dependencies
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ReactCodeInput from "react-verification-code-input";
 
 // Types
@@ -9,9 +9,22 @@ import { InputCodeProps } from "./types";
 import { Layout } from "./styles";
 
 function InputCode({ fields, loading, onComplete, autoFocus = false }: InputCodeProps) {
+  // Hooks
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      const input = inputRef.current as any;
+      if (input.iRefs.length && autoFocus) {
+        input.iRefs[0].current.focus();
+      }
+    }
+  }, [inputRef]);
+
   return (
     <Layout>
       <ReactCodeInput
+        ref={inputRef}
         className={"input-box"}
         fields={fields}
         loading={loading}
