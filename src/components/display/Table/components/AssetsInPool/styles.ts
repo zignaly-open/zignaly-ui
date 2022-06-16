174cea04ea2d81b2b5eb40143a4d0a08a303746f
VetleFell <vetle.stubberud@gmail.com>
2022-06-16 15:31:29 +0100

    fix(tables): changes to alignment of elements in user and market

--- src/components/display/Table/components/AssetsInPool/styles.ts
+++ src/components/display/Table/components/AssetsInPool/styles.ts
@@ -1,19 +1,4 @@
-
-   
 import styled from "styled-components";
-
-export const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: start;
-  text-align: start;
-`;
-
-export const Column = styled.div`
-  display: grid;
-  justify-content: center;
-`;
 
 export const Icon = styled.div`
   margin-left: 8px;
